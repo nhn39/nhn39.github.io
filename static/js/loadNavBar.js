7fe22beb07923494726eed332bfe37a65acd33e8
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-04-30 18:45:22 -0400

    update

--- static/js/loadNavBar.js
+++ static/js/loadNavBar.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-        })
-        .catch(error => console.error('Error loading the navbar:', error));
+    const navbar = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Nam Nguyen</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="projects.html">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>`;
+    document.body.insertAdjacentHTML('afterbegin', navbar);
 });
