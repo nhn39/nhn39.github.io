a45254692c4bb321a5b1d63f1c95a87f51d7fdf1
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-04-30 18:36:58 -0400

    update

--- static/js/loadNavBar.js
+++ static/js/loadNavBar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
+    fetch('../navbar.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
