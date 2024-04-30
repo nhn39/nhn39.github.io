3eebc60ea08798f2a2f8f2b140a25c872c5570aa
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-04-30 18:29:35 -0400

    update navbar html and js

---
+++ static/js/loadNavBar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+        })
+        .catch(error => console.error('Error loading the navbar:', error));
+});
