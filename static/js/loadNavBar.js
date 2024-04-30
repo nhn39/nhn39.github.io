20d3f4e1799bc8784f43327b49c34f3cc53f95fe
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-04-30 19:48:44 -0400

    update interface using javascript to have nav bar

--- static/js/loadNavBar.js
+++ static/js/loadNavBar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navbar = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Nam Nguyen</a>
+        <a class="navbar-brand" href="index.html">Nam Nguyen</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
