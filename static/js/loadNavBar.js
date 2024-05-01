61cb45c4a200fbf6b43d2b84b7de44a7ca73285a
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-05-01 01:49:26 -0400

    update

--- static/js/loadNavBar.js
+++ static/js/loadNavBar.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.html">Nam Nguyen</a>
-        <div class="navbar-nav">
-            <ul class="navbar-nav">
+        <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="about.html">About</a>
                 </li>
@@ -19,8 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <li class="nav-item">
                     <a class="nav-link" href="contact.html">Contact</a>
                 </li>
-            </ul>
-        </div>
+        </ul>
     </nav>`;
     document.body.insertAdjacentHTML('afterbegin', navbar);
 });
