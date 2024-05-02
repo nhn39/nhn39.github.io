38fae365287bc471649568bfa3c028bcc0472cfd
hoangnamtbp <123501097+hoangnamtbp@users.noreply.github.com>
2024-05-02 00:50:38 -0400

    update

--- static/js/loadNavBar.js
+++ static/js/loadNavBar.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.html">Nam Nguyen</a>
-            <ul class="navbar-nav">
+            <ul class="navbar-nav" ml-auto>
                 <li class="nav-item">
                     <a class="nav-link" href="about.html">About</a>
                 </li>
